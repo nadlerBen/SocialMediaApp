d7eb9675bcaf9876ee8ce740c0580c7f29f5049f
bennadler <ben.nadler@campus.technion.ac.il>
2019-10-06 18:52:58 +0300

    first commit

---
+++ src/components/post/PostAPost.js
@@ -0,0 +1,137 @@
+import React, {Component, Fragment} from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import MyButton from '../util/MyButton';
+
+//MUI Stuff
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import EditIcon from '@material-ui/icons/Edit';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import AddIcon from '@material-ui/icons/Add';
+import CloseIcon from '@material-ui/icons/Close';
+
+//Redux Stuff
+import { connect } from 'react-redux';
+import { postAPost, clearErrors } from "../redux/actions/dataActions";
+import { DialogContentText } from "@material-ui/core";
+
+const styles = (theme) => ({
+    ...theme.spread,
+    submitButton: {
+        position: 'relative',
+        float: 'right',
+        marginTop: 10,
+        marginBottom: 10
+    },
+    progressSpinner: {
+        position: 'absolute'
+    },
+    closeButton: {
+        position: 'absolute',
+        left: '90%',
+        top: '1%'
+    }
+});
+
+class PostAPost extends Component {
+    state = {
+        open: false,
+        body: '',
+        errors: {}
+    };
+    componentWillReceiveProps(nextProps, nextContext) {
+        if(nextProps.UI.errors) {
+            this.setState({
+                errors: nextProps.UI.errors
+            })
+        }
+        if(!nextProps.UI.errors && !nextProps.UI.loading) {
+            this.setState({ body: '', open: false, errors: {}  });
+        }
+    }
+
+    handleOpen = () => {
+        this.setState({ open:  true });
+    };
+    handleClose = () => {
+        this.props.clearErrors();
+        this.setState({ open: false, errors: {} });
+    };
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.postAPost({ body: this.state.body });
+    };
+    render() {
+        const { errors } = this.state;
+        const { classes , UI: { loading }} = this.props;
+        return (
+            <Fragment>
+                <MyButton onClick={this.handleOpen} tip='Post a post!'>
+                    <AddIcon/>
+                </MyButton>
+                    <Dialog
+                        open={this.state.open}
+                        onClose={this.handleClose}
+                        fullWidth
+                        maxWidth='sm'
+                    >
+                        <MyButton
+                            tip='Close'
+                            onClick={this.handleClose}
+                            tipClassName={classes.closeButton}>
+                            <CloseIcon/>
+                        </MyButton>
+                        <DialogTitle>Add a new post</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Share your thought with us
+                            </DialogContentText>
+                            <form onSubmit={this.handleSubmit}>
+                                <TextField
+                                    name='body'
+                                    type='text'
+                                    label='Post'
+                                    multiline
+                                    rows='3'
+                                    placeholder='Post your thoughts'
+                                    error={errors.body ? true : false}
+                                    helperText={errors.body}
+                                    className={classes.textField}
+                                    onChange={this.handleChange}
+                                    fullWidth
+                                />
+                                <Button type='submit' variant='contained' color='primary'
+                                        className={classes.submitButton} disabled={loading}>
+                                    Submit
+                                    { loading && (
+                                        <CircularProgress size={30} className={classes.progressSpinner}/>
+                                    )}
+                                </Button>
+                            </form>
+                        </DialogContent>
+                    </Dialog>
+            </Fragment>
+        )
+    }
+}
+
+PostAPost.propTypes = {
+    postAPost: PropTypes.func.isRequired,
+    clearErrors: PropTypes.func.isRequired,
+    UI: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    UI: state.UI
+});
+
+export default connect(mapStateToProps, { postAPost, clearErrors })(withStyles(styles)(PostAPost));
